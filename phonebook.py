985d1f1956c1c740544a1e1809da1e2599cca7e3
kadimasum <kadimasam@gmail.com>
2024-02-14 23:15:56 +0300

    Add more tests and crud operations

--- phonebook.py
+++ phonebook.py
@@ -1,6 +1,6 @@
 class Phonebook:
+
     contacts = []
-    id = 0
 
     def __init__(self, first_name:str, last_name:str, phone_number:str, email:str) -> None:
         self.first_name   = first_name
@@ -9,14 +9,34 @@ class Phonebook:
         self.email        = email
 
     def save(self):
-        if len(Phonebook.contacts) == 0:
-            self.id = 1
-            Phonebook.id = 1
+        if len(Phonebook.contacts) != 0:
+            self.id = len(Phonebook.contacts) + 1
         else:
-            Phonebook.id += 1
-            self.id = Phonebook.id
-            
-        self.contacts.append(self)
+            self.id = 1
+        Phonebook.contacts.append(self)
+         
 
     def read_all_contacts():
         return Phonebook.contacts
+    
+
+    def delete_contact(id: int):
+        for contact in Phonebook.contacts:
+            if contact.id == id:
+                del Phonebook.contacts[id - 1]
+    
+    def filter_by_first_name(name: str):
+        for contact in Phonebook.contacts:
+            if contact.first_name == name:
+                return contact.__dict__
+            
+    def update_name(self, id: int, new_first_name: str, new_last_name: str):
+        for contact in Phonebook.contacts:
+            if contact.id == id:
+                contact.first_name = new_first_name
+                contact.last_name = new_last_name
+
+    def update_number(self, id: int, new_number: str):
+        for contact in Phonebook.contacts:
+            if contact.id == id:
+                contact.phone_number = new_number
--- phonebook_test.py
+++ phonebook_test.py
@@ -5,6 +5,8 @@ class Phonebook_Test(unittest.TestCase):
 
     def setUp(self) -> None:
         self.person_x = Phonebook("John", "Doe", "07123345", "johndoe@gmail.com")
+        self.person_y = Phonebook("Brian", "Doe", "07123345", "briandoe@gmail.com")
+        self.person_z = Phonebook("Jane", "Doe", "07123345", "janedoe@gmail.com")
 
     def tearDown(self) -> None:
         Phonebook.contacts.clear()
@@ -18,8 +20,6 @@ class Phonebook_Test(unittest.TestCase):
         self.assertEqual("John", Phonebook.contacts[0].first_name)
     
     def test_read_all_contacts(self):
-        self.person_y = Phonebook("Brian", "Doe", "07123345", "briandoe@gmail.com")
-        self.person_z = Phonebook("Jane", "Doe", "07123345", "janedoe@gmail.com")
         self.person_y.save()
         self.person_z.save()
         self.person_x.save()
@@ -29,6 +29,38 @@ class Phonebook_Test(unittest.TestCase):
         self.assertEqual("John", Phonebook.contacts[2].first_name)
         self.assertEqual(3, len(Phonebook.read_all_contacts()))
 
+    def test_delete_contact(self):
+        self.person_y.save()
+        self.person_z.save()
+
+        Phonebook.delete_contact(1)
+        
+        self.assertEqual(1, len(Phonebook.contacts))
+        self.assertEqual(2, Phonebook.contacts[0].id)
+
+    def test_filter_by_first_name(self):
+        self.person_y.save()
+        self.person_z.save()
+        self.person_x.save()
+
+        self.assertTrue(self.person_x.__dict__, Phonebook.filter_by_first_name("John"))
+        self.assertTrue(self.person_y.__dict__, Phonebook.filter_by_first_name("Brian"))
+        self.assertTrue(self.person_z.__dict__, Phonebook.filter_by_first_name("Jane"))
+
+
+    def test_update_name(self):
+        self.person_y.save()
+        self.person_y.update_name(1,"Bruno", "Jackson" )
+        
+        self.assertEqual("Bruno", self.person_y.first_name)
+        self.assertEqual("Jackson", self.person_y.last_name)
+
+    def test_update_number(self):
+        self.person_x.save()
+
+        self.person_x.update_number(1, "0724558667")
+
+        self.assertEqual("0724558667", self.person_x.phone_number)
 
 
 
